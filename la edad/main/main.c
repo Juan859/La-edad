4a92082a16914f0c75dd300efad3bde56a127906
Juan C. Mate Peña <52606424+Juan859@users.noreply.github.com>
2021-03-25 11:12:53 -0400

    C/C++
    La edad

---
+++ la edad/main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int edad;
+    printf("introduzca su edad");
+    scanf("%i",&edad);
+    if(edad>18)
+{
+printf("Es mayor de edad: %i",edad);
+    }
+    else
+{
+ printf("Es menor de edad:%i",edad);
+
+ return 0;
+
+    }
+}
---
+++ la edad/sumas de tres num/la suma de tres num.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+  float main()
+  {
+
+float num1=8;
+float num2=9;
+float num3=7;
+float suma;
+suma= num1 + num2 + num3;
+printf("resultado es:%f",suma);
+return 0;
+
+
+  }
